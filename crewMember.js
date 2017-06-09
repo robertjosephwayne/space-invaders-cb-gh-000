4419010f1604a02adb2e468fae2798c3a0909c36
Robert Wayne <robertjwayne@gmail.com>
2017-06-09 12:39:03 -0400

    Add CrewMember class

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,31 @@
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = 'Looking for a Rig';
+  }
 
+  setCurrentShip(spaceship) {
+    this.currentShip = spaceship;
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip.constructor.name === 'Spaceship') {
+      this.currentShip.toggleWarpDrive(this);
+    }
+    return 'had no effect';
+  }
+
+  setsInvisibility() {
+    if (this.currentShip.constructor.name === 'Spaceship') {
+      this.currentShip.toggleInvisibility(this);
+    }
+    return 'had no effect';
+  }
+
+  chargePhasers() {
+    if (this.currentShip.constructor.name === 'Spaceship') {
+      this.currentShip.chargePhasers(this);
+    }
+    return 'had no effect';
+  }
+}
